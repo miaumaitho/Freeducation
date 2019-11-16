774905580b7b9524558fb844485422d70e06e100
Luis Eduardo Arellano Leyva <are.eduardo0930@gmail.com>
2019-11-16 04:41:18 -0600

    Modulos/descarga

--- Hakacthon19/app/src/main/java/com/example/hakacthon19/AsesoresMain.java
+++ Hakacthon19/app/src/main/java/com/example/hakacthon19/AsesoresMain.java
@@ -12,20 +12,17 @@ public class AsesoresMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.asesores_main);
-        //
 
-        Button downloadButton= (Button) findViewById(R.id.btnModulos);
-        downloadButton.setOnClickListener(new View.OnClickListener() {
+        Button btn = (Button)findViewById(R.id.btnModulos);
+
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                /*String url = "http://www.cursosinea.conevyt.org.mx/descargables/mevyt_pdfs/matematicas_empezar/01_matematicas_pe_libro.pdf";
-
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse(url));
-                startActivity(i);*/
+                startActivity(new Intent(AsesoresMain.this, Modulos.class));
             }
         });
+
+
     }
 
 
--- Hakacthon19/app/src/main/java/com/example/hakacthon19/Modulos.java
+++ Hakacthon19/app/src/main/java/com/example/hakacthon19/Modulos.java
@@ -1,4 +1,54 @@
 package com.example.hakacthon19;
 
-public class Modulos {
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Modulos extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.modulos);
+
+        ImageButton downloadLetras=findViewById(R.id.ibtnPalabra);
+        ImageButton downloadMates=findViewById(R.id.ibtnMates);
+        ImageButton downloadFiguras=findViewById(R.id.ibtnFiguras);
+
+        downloadLetras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.cursosinea.conevyt.org.mx/descargables/mevyt_pdfs/la_palabra/01_lp_libro.pdf";
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        downloadMates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.cursosinea.conevyt.org.mx/descargables/mevyt_pdfs/matematicas_empezar/01_matematicas_pe_libro.pdf";
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        downloadFiguras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.cursosinea.conevyt.org.mx/descargables/mevyt_pdfs/figuras_medidas/01_figuras_medidas_libro.pdf";
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+    }
+
 }
